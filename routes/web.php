a26d130390f55080139997fd69530703ff773e32
xu qian su <s3a932003@student.ncut.edu.tw>
2022-11-28 20:52:39 +0800

    改寫路由，確認原本路由是否改變

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::resource('photos', PhotoController::class)->only([
+/*Route::resource('photos', PhotoController::class)->only([
     'index','show','store','update','destroy'
-]);
+]);*/
+Route::get('photos', [PhotoController::class, 'index'])->name("photos.index");
+Route::get('photos/{photo}', [PhotoController::class, 'show'])->name("photos.show");
+Route::post('photos', [PhotoController::class, 'store'])->name("photos.store");
+Route::patch('photos/{photo}', [PhotoController::class, 'update'])->name("photos.update");
+Route::delete('photos/{photo}', [PhotoController::class, 'destroy'])->name("photos.destroy");
+
 Route::get('/', function () {
     return view('welcome');
 });
